c72908de04b7914bd75818f312630c2e45977602
Matt Perry <mperry37@alaska.edu>
2022-01-20 21:43:47 -0900

    chore: moving tests to proper dir

--- tests/test.py
+++ tests/test.py
@@ -1,1 +1,11 @@
-#should write some tests
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://root:example@172.23.144.1:8081/?authSource=admin")
+
+db = client.authn
+
+collection = db.collection
+
+cursor = collection.find({})
+for document in cursor:
+        print(document)
