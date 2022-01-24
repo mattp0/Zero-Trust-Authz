e21bdbaaeb2c238d93a9dc5748fe0846bc32642b
Matt Perry <mperry37@alaska.edu>
2022-01-23 18:52:17 -0900

    feat: add build_json user helper
    function builds a json user using the infomation passed into user
    
    depends on the oauth token

--- authz/src/helper.py
+++ authz/src/helper.py
@@ -2,9 +2,16 @@ import requests
 def build_user(user):
     return user
 
-def add_user(user, url):
-    full = url+"/create"
-    
+def build_user_json(user):
+    json_user = {
+        "uuid": f"{user['sub']}",
+        "sub": f"{user['sub']}",
+        "name": f"{user['name']}",
+        "permissions": base_permissions
+    }
+    return json.dumps(json_user)
+
+def create_json_user(user):
     x = requests.post(full, data=user)
     if x.status_code == 200:
         return True
