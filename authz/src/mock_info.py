672e5c73cc4cc8bcd564e62db8f877bb532d51a1
Matt Perry <mperry37@alaska.edu>
2022-02-05 13:26:44 -0900

    feat: add some mock info for testing mixin objects

---
+++ authz/src/mock_info.py
@@ -0,0 +1,49 @@
+meta = {
+    "redirect_uris": "localhost",
+    "token_endpoint_auth_method": "mattrocks",
+    "client_secret_basic": "mattrocks",
+    "grant_types": ["matt","rocks"],
+    "response_types": ["matt","rocks"],
+    "client_name": "matt rocks",
+    "client_uri": "matt rocks",
+    "logo_uri": "matt rocks",
+    "scope": "matt rocks",
+    "contacts": ["matt","rocks"],
+    "tos_uri": "matt rocks",
+    "policy_uri": "matt rocks",
+    "jwks_uri": "matt rocks",
+    "jwks": ["matt","rocks"],
+    "software_id": "matt rocks",
+    "software_version": "matt",
+}
+
+client_info = {
+    "client_id":"123456789",
+    "client_secret":"secret",
+    "client_id_issued_at": 1644093866,
+    "client_secret_expires_at": 1644193866,
+}
+
+token_info = {
+    "client_id":"123456789",
+    "token_type":"secret",
+    "access_token":"secret",
+    "refresh_token":"secret",
+    "scope":"secret",
+    "issued_at": 1644093866,
+    "access_token_revoked_at": 1644193866,
+    "refresh_token_revoked_at": 1644193866,
+    "expires_in": 300,
+}
+
+auth_code_info = {
+    "code":"123456789",
+    "client_id":"secret",
+    "redirect_uri":"secret",
+    "response_type":"secret",
+    "scope":"secret",
+    "nonce":"secret",
+    "auth_time": 1644193866,
+    "code_challenge":"test",
+    "code_challenge_method":"test",
+}
