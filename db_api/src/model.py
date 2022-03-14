4423f7a64409849df7ff248fa17956e05f3f3b37
Matt Perry <mperry37@alaska.edu>
2022-03-14 15:47:12 -0800

    feat: add new models for token, and clients

--- db_api/src/model.py
+++ db_api/src/model.py
@@ -87,3 +87,59 @@ class ClientModel(BaseModel):
                 "_client_metadata": ["web", "admin", "chat"],
             }
         }
+class UpdateClientModel(BaseModel):
+    client_secret: Optional[str]
+    client_secret: Optional[str]
+    client_id_issued_at: Optional[int]
+    client_secret_expires_at: Optional[int]
+    _client_metadata: Optional[List[str]]
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "client_id": "1231231231",
+                "client_secret": "joe bob",
+                "client_id_issued_at": 45123,
+                "client_secret_expires_at": 84000,
+                "_client_metadata": ["web", "admin", "chat"],
+            }
+        }
+
+class TokenObject(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid Object")
+        return ObjectId(v)
+    
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+class TokenModel(BaseModel):
+    client_id: TokenObject = Field(default_factory=TokenObject, alias="_id")
+    client_secret: str = Field(...)
+    client_id_issued_at: int = Field(...)
+    client_secret_expires_at: int = Field(...)
+    _client_metadata: List[str] = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "client_id": "1231231231",
+                "client_secret": "joe bob",
+                "client_id_issued_at": 45123,
+                "client_secret_expires_at": 84000,
+                "_client_metadata": ["web", "admin", "chat"],
+            }
+        }
