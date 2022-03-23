027cf8266d346f37657ffe5bdfad4571dfa41fa1
Matt Perry <mperry37@alaska.edu>
2022-03-22 16:26:24 -0800

    feat: extend dbapi testing

--- tests/db_test.py
+++ tests/db_test.py
@@ -1,7 +1,9 @@
 import requests
 import json
+import secrets
+import time
 
-db_api_url = "http://172.23.144.1:8070"
+db_api_url = "http://192.168.40.189:8070"
 
 test_user = {
         "sub": "1057831111111",
@@ -10,7 +12,7 @@ test_user = {
 }
 
 test_perms = {
-    "permissions": ["admin"]
+    "permissions": ["web"]
 }
 
 def user_exists(user):
@@ -26,8 +28,8 @@ def user_exists(user):
 
 def create_json_user(user):
     print("testing create user")
-    create_user_endpoint = db_api_url+"/create"
-    response = requests.post(create_user_endpoint, data=user)
+    create_user_endpoint = db_api_url+"/user/create"
+    response = requests.post(create_user_endpoint, data=json.dumps(user))
     if response.status_code == 201:
         print(response.content)
         return True, response.content
@@ -36,23 +38,89 @@ def create_json_user(user):
 def update_user(user, new_user):
     print(f"testing update user, with info:{user}, to {new_user}")
     user = json.loads(str(user, "utf-8"))
-    update_endpoint = db_api_url + "/update/" + user["_id"]
+    update_endpoint = db_api_url + "/user/update/" + user["_id"]
     response = requests.put(update_endpoint, data=json.dumps(new_user))
     if response.status_code == 202 or response.status_code == 200:
         return True, response.content
     return False, None
 
+def create_client():
+    data = {
+        "client_secret": secrets.token_urlsafe(32),
+        "client_id_issued_at": int(time.time()),
+        "client_secret_expires_at": int(time.time()) + int(84_000),
+        "_client_metadata": ["made by a test"]
+    }
+    update_endpoint = db_api_url + "/client/create"
+    print("preparing to send post request")
+    response = requests.post(update_endpoint, data=json.dumps(data))
+    print("sent post")
+    if response.status_code == 202 or response.status_code == 200:
+        return True, response.content
+    return False, None
+
+def create_authcode():
+    data = {
+        "client_id": "45123",
+        "redirect_uri": "http://mperry.io",
+        "scope": "openid profile email",
+        "grant_user": "21323423245",
+        "nonce": "1212134"
+    }
+    update_endpoint = db_api_url + "/authcode/create"
+    response = requests.post(update_endpoint, data=json.dumps(data))
+    if response.status_code == 202 or response.status_code == 200:
+        return True, response.content
+    return False, None
+
+def get_authcode(code):
+    update_endpoint = db_api_url + "/authcode/" + code
+    response = requests.get(update_endpoint)
+    if response.status_code == 200:
+        return True, response.content
+    return False, None
+
+def delete_authcode(code):
+    update_endpoint = db_api_url + "/authcode/delete/" + code
+    response = requests.get(update_endpoint)
+    if response.status_code == 200:
+        return True
+    return False
+
 if __name__ == "__main__":
     res, user = user_exists(test_user)
     print("testing if a user can be made twice")
-    dupe_res, dupe_user = create_json_user(json.dumps(test_user))
+    dupe_res, dupe_user = create_json_user(test_user)
+    print(dupe_user)
     if dupe_res:
         print("something failed")
     if res:
         res, user = update_user(user, test_perms)
         print(user)
     else:
-        res, user = create_json_user(json.dumps(test_user))
+        res, user = create_json_user(test_user)
+        print(user)
         if res:
             res, user = update_user(user, test_perms)
             print(user)
+    print("testing client creation")
+    res, content = create_client()
+    print(res, content)
+    print("testing authcode creation")
+    res, content = create_authcode()
+    print(res, content)
+    data = json.loads(content)
+    print("get getter for authcode")
+    res, content = get_authcode(data['_id'])
+    print(res, content)
+    print(json.loads(content))
+
+    print("get delete for authcode")
+    res = delete_authcode(data['_id'])
+    print(res)
+
+    print("get getter for authcode")
+    res, content = get_authcode(data['_id'])
+    print(res, content)
+    if content is not None:
+        print(json.loads(content))
