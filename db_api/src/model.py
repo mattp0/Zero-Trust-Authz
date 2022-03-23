d604c57c184725e4256a11f399ccfc64624d8be2
Matt Perry <mperry37@alaska.edu>
2022-03-22 16:26:15 -0800

    feat: correct model issues

--- db_api/src/model.py
+++ db_api/src/model.py
@@ -80,27 +80,6 @@ class ClientModel(BaseModel):
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "client_id": "1231231231",
-                "client_secret": "joe bob",
-                "client_id_issued_at": 45123,
-                "client_secret_expires_at": 84000,
-                "_client_metadata": ["web", "admin", "chat"],
-            }
-        }
-class UpdateClientModel(BaseModel):
-    client_secret: Optional[str]
-    client_secret: Optional[str]
-    client_id_issued_at: Optional[int]
-    client_secret_expires_at: Optional[int]
-    _client_metadata: Optional[List[str]]
-
-    class Config:
-        allow_population_by_field_name = True
-        arbitrary_types_allowed = True
-        json_encoders = {ObjectId: str}
-        schema_extra = {
-            "example": {
-                "client_id": "1231231231",
                 "client_secret": "joe bob",
                 "client_id_issued_at": 45123,
                 "client_secret_expires_at": 84000,
@@ -124,11 +103,12 @@ class TokenObject(ObjectId):
         field_schema.update(type="string")
 
 class TokenModel(BaseModel):
-    client_id: TokenObject = Field(default_factory=TokenObject, alias="_id")
-    client_secret: str = Field(...)
-    client_id_issued_at: int = Field(...)
-    client_secret_expires_at: int = Field(...)
-    _client_metadata: List[str] = Field(...)
+    id: TokenObject = Field(default_factory=TokenObject, alias="_id")
+    token_id: str = Field(...)
+    token_type: str = Field(...)
+    scope: str = Field(...)
+    access_token: str = Field(...)
+    expires_in: int = Field(...)
 
     class Config:
         allow_population_by_field_name = True
@@ -136,10 +116,47 @@ class TokenModel(BaseModel):
         json_encoders = {ObjectId: str}
         schema_extra = {
             "example": {
-                "client_id": "1231231231",
-                "client_secret": "joe bob",
-                "client_id_issued_at": 45123,
-                "client_secret_expires_at": 84000,
-                "_client_metadata": ["web", "admin", "chat"],
+                "token_id": "sadfasdfasdfwerdadf21342wsfda",
+                "token_type": "Bearer",
+                "scope": "openid profile email",
+                "access_token": 'swerasdfwewdasdf',
+                "expires_in": 84000,
+            }
+        }
+
+class AuthCodeObject(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+    
+    @classmethod
+    def validate(cls, v):
+        if not ObjectId.is_valid(v):
+            raise ValueError("Invalid Object")
+        return ObjectId(v)
+    
+    @classmethod
+    def __modify_schema__(cls, field_schema):
+        field_schema.update(type="string")
+
+class AuthCodeModel(BaseModel):
+    code: AuthCodeObject = Field(default_factory=AuthCodeObject, alias="_id")
+    client_id: str = Field(...)
+    redirect_uri: str = Field(...)
+    scope: str = Field(...)
+    grant_user: str = Field(...)
+    nonce : str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "client_id": "45123",
+                "redirect_uri": "http://mperry.io",
+                "scope": "openid profile email",
+                "grant_user": "21323423245",
+                "nonce": "1212134"
             }
         }
