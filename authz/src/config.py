b135390c495de44c04aa03211aaec4f36caacc42
Matt Perry <mperry37@alaska.edu>
2022-03-26 00:03:23 -0800

    feat: update config file

--- authz/src/config.py
+++ authz/src/config.py
@@ -1,3 +1,11 @@
 db_api_url = "http://192.168.40.189:8070"
 base_permissions = ["test"]
 email_scope = "alaska.edu"
+domain = "mperry.io"
+
+DUMMY_JWT_CONFIG = {
+    'key': 'secret-key',
+    'alg': 'HS256',
+    'iss': 'https://mperry.io',
+    'exp': 3600,
+}
