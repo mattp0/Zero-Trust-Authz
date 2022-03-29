5f301eaf87e4a30beb79034005e6130363bd39e6
Matt Perry <mperry37@alaska.edu>
2022-03-28 20:44:52 -0800

    feat: add configurable redirect url

--- authz/src/config.py
+++ authz/src/config.py
@@ -2,6 +2,7 @@ db_api_url = "http://authz-dbapi"
 base_permissions = ["test"]
 domain = "mperry.io"
 allowed_permission = "web"
+oauth_redirect_url = "http://auth.mperry.io/authorize"
 
 DUMMY_JWT_CONFIG = {
     'key': 'secret-key',
